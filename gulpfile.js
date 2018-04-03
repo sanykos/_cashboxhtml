1cc49a8f439684a85d5b4c7fa47bfca735fea7d0
Alexander Kosykh <kosih2009@yandex.ru>
2018-04-03 18:59:37 +0300

    add fonts

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,17 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglifyjs');
+    uglify = require('gulp-uglifyjs'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    cache = require('gulp-cache'),
+    autoprefixer = require('gulp-autoprefixer');
 
 
 gulp.task('sass', function () {
     return gulp.src('app/sass/**/*.sass')
         .pipe(sass())
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'],{cascade:true}))
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({stream:true}))
 });
@@ -21,6 +26,23 @@ gulp.task('scripts', function() {
 });
 
 
+gulp.task('clear', function () {
+    return cache.clearAll();
+});
+
+
+gulp.task('img', function () {
+   return gulp.src('app/img/**/*')
+       .pipe(cache(imagemin({
+           interlaced:true,
+           progressive: true,
+           svgoplugins: [{removeViewBox:false}],
+           use:[pngquant()]
+       })))
+       .pipe(gulp.dest('app/img'))
+});
+
+
 gulp.task('browser-sync', function () {
     browserSync({
         server: {
@@ -31,7 +53,7 @@ gulp.task('browser-sync', function () {
     });
 });
 
-gulp.task('watch', ['browser-sync', 'sass', 'scripts'], function(){
+gulp.task('watch', ['browser-sync', 'img', 'sass', 'scripts'], function(){
     gulp.watch('app/sass/**/*.sass', ['sass']);
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
