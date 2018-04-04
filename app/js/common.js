54e9341a0f2979afa3153beb8ec06fa907a0fa50
Alexander Kosykh <kosih2009@yandex.ru>
2018-04-04 10:37:43 +0300

    toggle menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,16 @@
 $(function () {
 
+    $('.icon').on('click', function () {
+       $(this).toggleClass('active');
+    if(!$(this).hasClass('active')){
+        $('.wrapper-menu').slideUp();
+    } else if($(this).hasClass('active')){
+        $('.wrapper-menu').slideDown().css({
+            display: "flex"
+        });
+
+    }
+
+    });
+
 });
